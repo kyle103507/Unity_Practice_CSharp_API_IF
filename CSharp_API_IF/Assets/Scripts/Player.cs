e13b8dcb0b6659bcc43908618c744c10a6abd245
kyle103507 <61506510+kyle103507@users.noreply.github.com>
2020-07-16 20:04:00 +0800

    人物控制器

---
+++ CSharp_API_IF/Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Player: MonoBehaviour
+{
+    public Animator air;
+
+    /// <summary>
+    /// 角色走路:走路動畫
+    /// </summary>
+    private void Walk()
+    {
+       bool key = Input.GetKey(KeyCode.W);
+        air.SetBool("走路開關",key);
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        Walk();
+    }
+
+}
